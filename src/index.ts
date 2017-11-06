ad8f3475605a3fdbb7fef1e5375e9663d1870e2f
Junya.K <kani@AMBASSADOR.g01.fujitsu.local>
2017-11-06 12:38:35 +0900

    once a two hours

--- src/index.ts
+++ src/index.ts
@@ -79,7 +79,7 @@ const timer = setInterval(()=>{
   if(m.isAfter(am7) && m.isBefore(pm23)) {
     postTweet();
   }
-}, 1000 * 60 /** 60*/ * 2);
+}, 1000 * 60 * 60 * 2);
 
 // 終了時にunsubとか
 process.on("SIGINT", () => {
